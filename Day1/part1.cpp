af19ade6ef460b43c05bbe5c017b86de425ff1ec
Ashish Kumar <ashishkumar901336@gmail.com>
2024-12-01 15:38:20 +0530

    init

---
+++ Day1/part1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> v1, v2;
+    int n1, n2;
+
+    while (cin >> n1 >> n2) {
+        v1.push_back(n1);
+        v2.push_back(n2);
+    }
+
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+
+    int res = 0;
+    for (int i = 0; i < v1.size(); i++) res += abs(v1[i] - v2[i]);
+
+    cout << res << endl;
+
+    return 0;
+}
---
+++ Day1/part2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> v1, v2;
+    int n1, n2;
+
+    while (cin >> n1 >> n2) {
+        v1.push_back(n1);
+        v2.push_back(n2);
+    }
+
+    unordered_map<int, int> mp;
+    for (int i = 0; i < v2.size(); i++) mp[v2[i]]++;
+
+    int res = 0;
+    for (int i = 0; i < v1.size(); i++) {
+        res += v1[i] * mp[v1[i]];
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
